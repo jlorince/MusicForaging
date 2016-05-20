e27db7da565ecc099e58db5a19394b4d0187f3d2
Jared Lorince <jaredlorince@Jareds-MacBook-Pro.local>
2016-05-20 11:48:38 -0400

    beginning main patch processing code

--- foragingAnalysis/debug.py
+++ foragingAnalysis/debug.py
@@ -4,13 +4,17 @@ import glob
 import pandas as pd
 
 
-
+def get_count(f):
+    return len(f.readlines())
 
 
 
 if __name__ == '__main__':
     files = glob.glob('/Users/jaredlorince/git/MusicForaging/testData/*tiny.txt')
     print files
-    pool = mp.Pool(2) # use 4 processes
+    pool = mp.Pool(32) # use 4 processes
+    n =0
     for r in pool.imap_unordered(raw_processor, files):
-        print r
+        n+= r
+    print n
+
