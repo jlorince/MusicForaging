a7e2ba0ebc8ffe6bcd0dfb3b3915bb451ee0de2f
Jared Lorince <jaredlorince@149-160-164-7.dhcp-bl.indiana.edu>
2016-06-06 16:32:14 -0400

    done file handling

---
+++ foragingAnalysis/MPA/log_processor.py
@@ -0,0 +1,19 @@
+import sys
+
+logfi = sys.argv[1]
+
+done = set()
+with open(logfi) as fin:
+    for line in fin:
+        if 'User' in line:
+            filename = line.strip().split()[-1]
+            done.add(filename[filename.rfind('/'):-1])
+
+with open('joblist.txt') as fin, open('joblist_partial.txt','w') as fout:
+    for line in fin:
+        line = line.strip()
+        if line[line.rfind('/'):-1] in done:
+            continue
+        else:
+            fout.write(line+'\n')
+
