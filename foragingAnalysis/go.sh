ba330c5a0f3b6443634c2709573653472fc8aaaa
Jared Lorince <jaredlorince@Jareds-MBP.hsd1.in.comcast.net>
2016-06-08 17:00:00 -0400

    patch length dist goodies

--- foragingAnalysis/go.sh
+++ foragingAnalysis/go.sh
@@ -16,7 +16,7 @@ for dist_thresh in `seq 0.1 0.1 0.9`; do
 
     for mpl in `seq 3 1 10`; do
 
-        python setup.py -p -n 32 --patch_len_dist shuffle --dist_thresh $dist_thresh --min_patch_length $mpl --session_thresh 0 --pickledir /N/dc2/scratch/jlorince/scrobbles_processed/ --feature_path /N/dc2/scratch/jlorince/support/features_190.npy --resultdir /N/dc2/scratch/jlorince/patch_len_dists/
+        python setup.py -p -f $1 --patch_len_dist shuffle --dist_thresh $dist_thresh --min_patch_length $mpl --session_thresh 0 --pickledir /N/dc2/scratch/jlorince/scrobbles_processed/ --feature_path /N/dc2/scratch/jlorince/support/features_190.npy --resultdir /N/dc2/scratch/jlorince/patch_len_dists/
 
     done
 
