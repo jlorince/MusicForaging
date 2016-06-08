a7b77f53cb3d8985ecc74d5ac668f8068b7d385e
Jared Lorince <jaredlorince@Jareds-MBP.hsd1.in.comcast.net>
2016-06-08 14:16:32 -0400

    code for patch length dists

--- foragingAnalysis/MPA/go_gcc.sh
+++ foragingAnalysis/MPA/go_gcc.sh
@@ -1,10 +1,10 @@
 for dist_thresh in `seq 0.05 0.05 0.9`; do
 
-    python setup.py -p --suppdir /Users/jaredlorince/git/MusicForaging/GenreModeling/data/ -n 32 --feature_path /home/jlorince/lda_tests_artists/features_190.npy --patch_len_dist both --dist_thresh $dist_thresh --min_patch_length 2
+    python setup.py -p --suppdir /Users/jaredlorince/git/MusicForaging/GenreModeling/data/ -n 32 --patch_len_dist both --dist_thresh $dist_thresh --min_patch_length 2
 
     for mpl in `seq 3 1 10`; do
 
-        python setup.py -p --suppdir /Users/jaredlorince/git/MusicForaging/GenreModeling/data/ -n 32 --feature_path /home/jlorince/lda_tests_artists/features_190.npy --patch_len_dist both --dist_thresh $dist_thresh --min_patch_length $mpl
+        python setup.py -p --suppdir /Users/jaredlorince/git/MusicForaging/GenreModeling/data/ -n 32 --patch_len_dist both --dist_thresh $dist_thresh --min_patch_length $mpl
 
     done
 
