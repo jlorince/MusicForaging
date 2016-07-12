f3793c74bcc19fce61111cb742cbcef0b0287ee5
Jared <jared.j.lorince@gmail.com>
2016-07-12 15:01:35 -0400

    random seq logging

--- foragingAnalysis/generate_seqs.py
+++ foragingAnalysis/generate_seqs.py
@@ -7,6 +7,7 @@ import pandas as pd
 from datetime import datetime,timedelta
 from pathos.multiprocessing import ProcessingPool as Pool
 from pathos.multiprocessing import cpu_count
+import logging
 
 # number of artists
 n_artists = 50000
@@ -20,22 +21,34 @@ feature_path =  '/home/jlorince/lda_tests_artists/features_190.npy' # '../GenreM
 artist_pop_path = '/home/jlorince/artist_pop'
 td_dist_path = '/home/jlorince/scrobble_td.npy'
 
+# setup logging
+
+logging.basicConfig(format='%(asctime)s %(message)s',level=logging.INFO)
+logging.warning('is when this event was logged.')
+
 # load feature data and generate distance matrix
 features = np.load(feature_path)[:n_artists]
+logging.info('features loaded')
 pw = squareform(pdist(features,metric='cosine'))
+logging.info('distance matrix calculated')
 pw = np.round(pw,2)
+logging.info('distance matrix rounded')
+
 
 # load overall artist popularity distribution
 pops = np.array([int(line.strip().split(',')[1]) for line in open(artist_pop_path)])[:n_artists]
 pops = pops / float(pops.sum())
+logging.info('features popularity info loaded')
 
 # load time gap distribution
 td =np.load(td_dist_path)
+logging.info('time distance data loaded')
 
 
 jump_dist_arr = np.loadtxt(jumpdist_path,delimiter=',',dtype=int)
 jump_dist_arr = jump_dist_arr/jump_dist_arr.sum(1,keepdims=True).astype(float)
 jumpdist = np.nanmean(jump_dist_arr,0)
+logging.info('jump distance data loaded')
 
 def draw(last):
     found = False
@@ -67,6 +80,7 @@ def genseq(idx):
     df = pd.DataFrame(result)
     df['block'] = ((df['artist_idx'].shift(1) != df['artist_idx']).astype(int).cumsum())-1
     df.to_pickle(str(idx)+'.pkl')
+    logging.info('idx {} complete'.format(idx))
 
 pool = Pool(cpu_count())
 indices = range(n)
