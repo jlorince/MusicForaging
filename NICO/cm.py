ca38d6ecb8bab5c135d5432b3a35f9e33c0aff94
Jared <jared.j.lorince@gmail.com>
2017-01-23 14:32:22 -0600

    formal survival analysis

--- NICO/cm.py
+++ NICO/cm.py
@@ -92,7 +92,7 @@ if __name__ == '__main__':
     mode = sys.argv[1]
     measure = sys.argv[2]
     if measure == 'hazard':
-        func = partial(hazard_curve,mode=mode,bandwidth=sys.argv[3])
+        func = partial(hazard_curve,mode=mode,bandwidth=int(sys.argv[3]))
     else:
         func = partial(survival_curve,mode=mode)
 
